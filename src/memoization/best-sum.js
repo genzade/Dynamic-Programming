04cce8e0b8384ffd2b4381de0ee026f163f3eca4
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-02-29 01:31:27 +0000

    Add bestSum function and tests

---
+++ src/memoization/best-sum.js
@@ -0,0 +1,62 @@
+// Write a function `howSum(targetSum, numbers)` that takes in a targetSum and
+// an array of numbers as arguments.
+//
+// The function should return an array containing the shortest combination of
+// numbers that add up to exactly the targetsum.
+//
+// If there is a tie for the shortest combination, you may return any one of
+// the shortest.
+
+// const bestSum = (targetSum, numbers, memo = {}, combos = []) => {
+//   if (targetSum in memo) return memo[targetSum];
+//   if (targetSum === 0) return [];
+//   if (targetSum < 0) return null;
+
+//   if (memo[targetSum] === undefined) {
+//     memo[targetSum] = [];
+//   }
+
+//   for (const num of numbers) {
+//     const nts = targetSum - num;
+//     const res = bestSum(nts, numbers, memo);
+//     console.log(memo);
+
+//     if (res !== null) {
+//       combos.push([...res, num]);
+//       memo[targetSum].push(...res, num);
+//       // console.log(memo);
+//       memo[targetSum];
+//     }
+//   }
+
+//   memo[targetSum] = null;
+//   return null;
+// };
+
+const bestSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  let shortestCombo = null;
+
+  for (const num of numbers) {
+    const nts = targetSum - num;
+    const res = bestSum(nts, numbers, memo);
+
+    if (res !== null) {
+      const combo = [...res, num];
+
+      if (shortestCombo === null || combo.length < shortestCombo.length) {
+        shortestCombo = combo;
+      }
+    }
+  }
+
+  memo[targetSum] = shortestCombo;
+  return shortestCombo;
+};
+
+module.exports = {
+  bestSum: bestSum,
+};
---
+++ test/memoization/best-sum.test.js
@@ -0,0 +1,39 @@
+const bs = require("../../src/memoization/best-sum");
+
+describe("Memoization::BestSum", () => {
+  it("returns correct result with targetSum = 7", () => {
+    const targetSum = 7;
+    const numbers = [5, 3, 4, 7];
+    const shortestCombo = [7];
+    expect(bs.bestSum(targetSum, numbers)).toStrictEqual(shortestCombo);
+  });
+
+  it("returns correct result with targetSum = 8", () => {
+    const targetSum = 8;
+    const numbers = [2, 3, 5];
+    // const shortestCombo = [3, 5];
+    const shortestCombo = [5, 3];
+    expect(bs.bestSum(targetSum, numbers)).toStrictEqual(shortestCombo);
+  });
+
+  it("returns correct result with targetSum = 8, numbers = [1, 4, 5]", () => {
+    const targetSum = 8;
+    const numbers = [1, 4, 5];
+    const shortestCombo = [4, 4];
+    expect(bs.bestSum(targetSum, numbers)).toStrictEqual(shortestCombo);
+  });
+
+  it("returns correct result with targetSum = 9", () => {
+    const targetSum = 9;
+    const numbers = [9];
+    const shortestCombo = [9];
+    expect(bs.bestSum(targetSum, numbers)).toStrictEqual(shortestCombo);
+  });
+
+  it("returns correct result with targetSum = 100", () => {
+    const targetSum = 100;
+    const numbers = [1, 2, 5, 25];
+    const shortestCombo = [25, 25, 25, 25];
+    expect(bs.bestSum(targetSum, numbers)).toStrictEqual(shortestCombo);
+  });
+});
